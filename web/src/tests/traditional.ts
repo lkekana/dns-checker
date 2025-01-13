df00105212c93f8a0ffca3a25f32c508827623d1
Lesedi Kekana <theoriginalles@gmail.com>
2025-01-13 16:20:36 +0200

    create init traditional DNS test

---
+++ web/src/tests/traditional.ts
@@ -0,0 +1,39 @@
+import { app, computer, os, events } from '@neutralinojs/lib';
+import { getOS, OS } from '../environment';
+
+export const runTraditionalDNSTest = async (): Promise<boolean> => {
+    const OStype = await getOS();
+    if (OStype === OS.macOS) {
+        // macOS
+
+        const cmd = `dig @${'8.8.8.8'} example.com | jc --dig`;
+        const digProc = await os.spawnProcess(cmd);
+        let digStdOut = '';
+        let digStdErr = '';
+        events.on('spawnedProcess', (evt) => {
+            if(digProc.id === evt.detail.id) {
+                switch(evt.detail.action) {
+                    case 'stdOut':
+                        console.log(evt.detail.data);
+                        digStdOut += evt.detail.data;
+                        break;
+                    case 'stdErr':
+                        console.error(evt.detail.data);
+                        digStdErr += evt.detail.data;
+                        break;
+                    case 'exit':
+                        console.log(`Dig process terminated with exit code: ${evt.detail.data}`);
+
+                        break;
+                }
+            }
+        });
+    }
+    else if (OStype === OS.Windows) {
+        // Windows
+    }
+    else if (OStype === OS.Linux) {
+        // Linux
+    }
+    return true;
+}
